bee5365ff9abd0a51b7fafd9102aa54f035d7cd0
Johan Quiroga <johan.c.quiroga@gmail.com>
2018-08-27 17:55:46 -0500

    Added Firebase Dynamic Links and routing for demo app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
 
 void main() => runApp(new MyApp());
 
@@ -10,8 +13,138 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: 'Baby names',
-      home: new MyHomePage(title: 'Baby Names Votes'),
+      routes: <String, WidgetBuilder>{
+        '/': (BuildContext context) => new _MainScreen(),
+        '/flutter-baby-names': (BuildContext context) =>
+            new MyHomePage(title: 'Baby Names Votes'),
+        '/helloworld': (BuildContext context) =>
+            new _DynamicLinkScreen(title: 'Hello World'),
+        '/firebase-flutter': (BuildContext context) =>
+            new _DynamicLinkScreen(title: 'Firebase Flutter Test'),
+      },
     );
+  }
+}
+
+class _MainScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => new _MainScreenState();
+}
+
+class _MainScreenState extends State<_MainScreen> with WidgetsBindingObserver {
+  String _linkMessage;
+  bool _isCreatingLink = false;
+  Timer _timerLink;
+  @override
+  BuildContext get context => super.context;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.resumed) {
+      _timerLink = new Timer(const Duration(milliseconds: 850), () {
+        _retrieveDynamicLink();
+      });
+    }
+  }
+
+  Future<void> _retrieveDynamicLink() async {
+    final PendingDynamicLinkData data =
+        await FirebaseDynamicLinks.instance.retrieveDynamicLink();
+    final Uri deepLink = data?.link;
+
+    if (deepLink != null) {
+      print(deepLink.toString());
+      Navigator.pushNamed(context, deepLink.path);
+    }
+  }
+
+  Future<void> _createDynamicLink(bool short) async {
+    setState(() {
+      _isCreatingLink = true;
+    });
+
+    final DynamicLinkParameters parameters = new DynamicLinkParameters(
+      domain: 'johanquiroga.page.link',
+      link: Uri.parse('http://johanquiroga.me/helloworld'),
+      androidParameters: new AndroidParameters(
+        packageName: 'com.example.firebasetest',
+        minimumVersion: 0,
+      ),
+      dynamicLinkParametersOptions: new DynamicLinkParametersOptions(
+        shortDynamicLinkPathLength: ShortDynamicLinkPathLength.short,
+      ),
+      // iosParameters: new IosParameters(
+      //   bundleId: 'com.google.FirebaseCppDynamicLinksTestApp.dev',
+      //   minimumVersion: '0',
+      // ),
+    );
+
+    Uri url;
+    if (short) {
+      final ShortDynamicLink shortLink = await parameters.buildShortLink();
+      url = shortLink.shortUrl;
+    } else {
+      url = await parameters.buildUrl();
+    }
+
+    setState(() {
+      _linkMessage = url.toString();
+      _isCreatingLink = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Material(
+      child: new Scaffold(
+        appBar: new AppBar(
+          title: const Text('Dynamic Links Example'),
+        ),
+        body: new Center(
+          child: new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new ButtonBar(
+                alignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  new RaisedButton(
+                    onPressed: !_isCreatingLink
+                        ? () => _createDynamicLink(false)
+                        : null,
+                    child: const Text('Get Long Link'),
+                  ),
+                  new RaisedButton(
+                    onPressed: !_isCreatingLink
+                        ? () => _createDynamicLink(true)
+                        : null,
+                    child: const Text('Get Short Link'),
+                  ),
+                ],
+              ),
+              new Text(
+                _linkMessage ?? '',
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    if (_timerLink != null) {
+      _timerLink.cancel();
+    }
+    super.dispose();
   }
 }
 
@@ -65,6 +198,26 @@ class MyHomePage extends StatelessWidget {
                 _buildListItem(context, snapshot.data.documents[index]),
           );
         },
+      ),
+    );
+  }
+}
+
+class _DynamicLinkScreen extends StatelessWidget {
+  const _DynamicLinkScreen({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return new Material(
+      child: new Scaffold(
+        appBar: new AppBar(
+          title: const Text('Hello World DeepLink'),
+        ),
+        body: new Center(
+          child: new Text(title),
+        ),
       ),
     );
   }
